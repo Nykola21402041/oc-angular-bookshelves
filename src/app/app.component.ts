6fae406fc4ee1519f628815a02b72c2e00d2c127
Nicolas Hoffmann <21402041@etu.unicaen.fr>
2019-01-09 14:44:12 +0100

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+
+  constructor() {
+    const config = {
+      apiKey: 'AIzaSyC5OYheRZP-NeIY3svELzqR4gx_tgiFLok',
+      authDomain: 'oc-angular-tutoriel.firebaseapp.com',
+      databaseURL: 'https://oc-angular-tutoriel.firebaseio.com',
+      projectId: 'oc-angular-tutoriel',
+      storageBucket: 'oc-angular-tutoriel.appspot.com',
+      messagingSenderId: '863859483434'
+    };
+    firebase.initializeApp(config);
+
+  }
+
 }
